7ed37d172fe44673ad05e615e06aca5456f0a62c
Madhumitha-007 <your_github_email@gmail.com>
2025-12-19 00:16:19 +0530

    Initial frontend setup

---
+++ src/Home.jsx
@@ -0,0 +1,15 @@
+// mern-frontend/src/Home.jsx - FINAL CODE (Currency Fix)
+
+import React from 'react';
+import './ProductStyles.css';
+
+const Home = () => {
+    return (
+        <div className="home-container" style={{ marginTop: '50px' }}>
+            <h1>Welcome to the E-Commerce Website</h1>
+            <p>Browse products using the navigation above.</p>
+        </div>
+    );
+};
+
+export default Home;
